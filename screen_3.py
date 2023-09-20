43810a3e8e70070c7843ec69a4d83c24f1c77720
work <adiissan415@gmail.com>
2023-09-20 12:44:42 +0300

    h

--- screen_3.py
+++ screen_3.py
@@ -1,22 +1,29 @@
 import always
 import pygame
 
+# text_font = pygame.font.SysFont(always.FONT_NAME, 50)
 
-# def draw_screen_1():
-#     always.SCREEN_1.fill(always.BACKGROUND_COLOR)
-#     pygame.draw.rect(screen1, always.SQUARE_COLOR, pygame.Rect(150, 275, 200, 200))
-#     pygame.draw.rect(screen1, always.SQUARE_COLOR, pygame.Rect(650, 275, 200, 200))
-#     pygame.display.flip()
-#
-#
-# while True:
-#     draw_screen_1()
 
 def open_screen_3():
     screen_3 = always.SCREEN_3
     dict_3 = {'running' : True}
     always.SCREEN_3.fill(always.BACKGROUND_COLOR)
-    pygame.draw.rect(screen_3, always.SQUARE_COLOR, pygame.Rect(50, 237.5, 187.5, 187.5))
+    pygame.draw.rect(screen_3, always.SQUARE_COLOR, pygame.Rect(50, 100, 250, 200))
+    pygame.draw.rect(screen_3, always.SQUARE_COLOR, pygame.Rect(350, 100, 250, 200))
+    pygame.draw.rect(screen_3, always.SQUARE_COLOR, pygame.Rect(650, 100, 250, 200))
+    pygame.draw.rect(screen_3, always.SQUARE_COLOR, pygame.Rect(950, 100, 250, 200))
+    pygame.draw.rect(screen_3, always.SQUARE_COLOR, pygame.Rect(115, 450, 250, 200))
+    pygame.draw.rect(screen_3, always.SQUARE_COLOR, pygame.Rect(465, 450, 250, 200))
+    pygame.draw.rect(screen_3, always.SQUARE_COLOR, pygame.Rect(815, 450, 250, 200))
+
+    pygame.draw.rect(screen_3, always.INER_SQUARE_COLOR, pygame.Rect(70, 120, 210, 160))
+    pygame.draw.rect(screen_3, always.INER_SQUARE_COLOR, pygame.Rect(370, 120, 210, 160))
+    pygame.draw.rect(screen_3, always.INER_SQUARE_COLOR, pygame.Rect(670, 120, 210, 160))
+    pygame.draw.rect(screen_3, always.INER_SQUARE_COLOR, pygame.Rect(970, 120, 210, 160))
+    pygame.draw.rect(screen_3, always.INER_SQUARE_COLOR, pygame.Rect(135, 470, 210, 160))
+    pygame.draw.rect(screen_3, always.INER_SQUARE_COLOR, pygame.Rect(485, 470, 210, 160))
+    pygame.draw.rect(screen_3, always.INER_SQUARE_COLOR, pygame.Rect(835, 470, 210, 160))
+
     pygame.display.flip()
     while dict_3['running']:
         for event in pygame.event.get():

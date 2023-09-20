b505d451b68ff7d10a30507d4cc561f083179c58
work <adiissan415@gmail.com>
2023-09-20 10:56:02 +0300

    h

---
+++ always.py
@@ -0,0 +1,9 @@
+import pygame
+WIDTH = 1000
+HEIGT = 750
+SCREEN_1 = pygame.display.set_mode((WIDTH, HEIGT))
+SCREEN_2 = pygame.display.set_mode((WIDTH, HEIGT))
+SCREEN_3 = pygame.display.set_mode((WIDTH, HEIGT))
+SCREEN_4 = pygame.display.set_mode((WIDTH, HEIGT))
+SCREEN_5 = pygame.display.set_mode((WIDTH, HEIGT))
+SCREEN_6 = pygame.display.set_mode((WIDTH, HEIGT))

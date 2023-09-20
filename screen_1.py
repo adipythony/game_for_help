bcd2cbb1ff723d34d89480252e5656f2057e6bdd
belle <bell.levi2406@gmail.com>
2023-09-20 11:22:45 +0300

    commit 3

--- screen_1.py
+++ screen_1.py
@@ -7,8 +7,7 @@ screen1 = always.SCREEN_1
 
 def draw_screen_1():
     always.SCREEN_1.fill(always.BACKGROUND_COLOR)
-    pygame.draw.rect(screen1, always.SQUARE_COLOR, pygame.Rect(150, 275, 200, 200))
-    pygame.draw.rect(screen1, always.SQUARE_COLOR, pygame.Rect(650, 275, 200, 200))
+    pygame.draw.rect(screen1, always.SQUARE_COLOR, pygame.Rect(150, 400, 700, 200))
     pygame.display.flip()
 
 

016beff2d6b3dce86e8e4e9b22ca7339595040e8
maya <mayush.benami@gmail.com>
2023-09-20 13:01:26 +0300

    add text

--- screen_2.py
+++ screen_2.py
@@ -11,21 +11,19 @@ def background():
     pygame.display.flip()
 
 def squares():
-    size = (450, 500)
-    helper_location = (100, 125)
-    needs_help_location = (700, 125)
     pygame.draw.rect(screen2, always.SQUARE_COLOR, pygame.Rect((100, 125), (450, 500)))
+    pygame.draw.rect(screen2, always.INER_SQUARE_COLOR, pygame.Rect((120, 145), (410, 460)))
+
     pygame.draw.rect(screen2, always.SQUARE_COLOR, pygame.Rect((700, 125), (450, 500)))
-    pygame.draw.rect(screen2, always.INER_SQUARE_COLOR, pygame.Rect((120,145), (410,460)))
     pygame.draw.rect(screen2, always.INER_SQUARE_COLOR, pygame.Rect((720, 145), (410, 460)))
     return
 
 def text():
     font = pygame.font.SysFont(always.FONT_NAME, 50)
     helping = font.render("Helper", True, always.FONT_COLOR)
-    screen2.blit(helping, (100, 125))
+    screen2.blit(helping, (250, 340))
     helped = font.render("Need Help", True, always.FONT_COLOR)
-    screen2.blit(helped, (700, 125))
+    screen2.blit(helped, (825, 340))
     return
 
 while True:

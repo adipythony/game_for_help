ce8409b61f2e424f712dd683368112cd9f66a60b
work <adiissan415@gmail.com>
2023-09-20 15:44:37 +0300

    h

--- screen_1.py
+++ screen_1.py
@@ -2,25 +2,42 @@ import always
 import pygame
 
 pygame.init()
-screen1 = always.SCREEN_1
+screen_1 = always.SCREEN_1
 
-text_font = pygame.font.SysFont(always.FONT_NAME, 50)
-RECT_WIDTH = 850
-RECT_LENGTH = 200
-RECT_LOCATION_X = (always.WIDTH - RECT_WIDTH) / 2
-RECT_LOCATION_Y = (always.HEIGHT - RECT_LENGTH) / 2
+text_font = pygame.font.SysFont(always.FONT_NAME, 48)
+RECT_WIDTH = 480
+RECT_LENGTH = 185
+RECT_LOCATION_X = 405
+RECT_LOCATION_Y = 430
 start = pygame.Rect((RECT_LOCATION_X, RECT_LOCATION_Y), (RECT_LOCATION_X + RECT_WIDTH, RECT_LOCATION_Y + RECT_LENGTH))
 
 
 def draw_text(text, font, text_col, x, y):
     txt = font.render(text, True, text_col)
-    screen1.blit(txt, (x, y))
+    screen_1.blit(txt, (x, y))
 
 
 def draw_screen_1():
+    pygame.init()
+    # dict_1 = {'running': True}
     always.SCREEN_1.fill(always.BACKGROUND_COLOR)
-    pygame.draw.rect(screen1, always.SQUARE_COLOR, pygame.Rect(RECT_LOCATION_X, RECT_LOCATION_Y, RECT_WIDTH, RECT_LENGTH))
-    draw_text("Begin Helping Process", text_font, always.FONT_COLOR, RECT_LOCATION_X + 200, RECT_LOCATION_Y + 70)
-    draw_text("Welcome to the helping center !", text_font, always.FONT_COLOR, 300, 100)
+    draw_outside_rect_1()
+    draw_iner_rect_1()
+    draw_text("Begin Helping Process", text_font, always.FONT_COLOR, RECT_LOCATION_X + 25, RECT_LOCATION_Y + 70)
+    text_font2 = pygame.font.SysFont(always.FONT_NAME, 60)
+    draw_text("Welcome to the helping center!", text_font2, always.FONT_COLOR, 250, 200)
     pygame.display.flip()
+    # while dict_1['running']:
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             dict_1['running'] = False
+    # return always.SCREEN_1
+
+def draw_outside_rect_1():
+    pygame.draw.rect(screen_1, always.SQUARE_COLOR, pygame.Rect(RECT_LOCATION_X, RECT_LOCATION_Y, RECT_WIDTH, RECT_LENGTH))
+    pygame.draw.rect(screen_1, always.SQUARE_COLOR, pygame.Rect((200, 100), (850, 270)))
+
+def draw_iner_rect_1():
+    pygame.draw.rect(screen_1, always.INER_SQUARE_COLOR, pygame.Rect(RECT_LOCATION_X + 20, RECT_LOCATION_Y + 20, RECT_WIDTH - 40, RECT_LENGTH - 40))
+    pygame.draw.rect(screen_1, always.INER_SQUARE_COLOR, pygame.Rect((220, 120), (810, 230)))
 

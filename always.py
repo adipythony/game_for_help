324ab5302696509e461d0fc0fff6be851c221492
belle <bell.levi2406@gmail.com>
2023-09-20 11:04:48 +0300

    commit 1

--- always.py
+++ always.py
@@ -1,9 +1,13 @@
 import pygame
 WIDTH = 1000
-HEIGT = 750
-SCREEN_1 = pygame.display.set_mode((WIDTH, HEIGT))
-SCREEN_2 = pygame.display.set_mode((WIDTH, HEIGT))
-SCREEN_3 = pygame.display.set_mode((WIDTH, HEIGT))
-SCREEN_4 = pygame.display.set_mode((WIDTH, HEIGT))
-SCREEN_5 = pygame.display.set_mode((WIDTH, HEIGT))
-SCREEN_6 = pygame.display.set_mode((WIDTH, HEIGT))
+HEIGHT = 750
+SCREEN_1 = pygame.display.set_mode((WIDTH, HEIGHT))
+SCREEN_2 = pygame.display.set_mode((WIDTH, HEIGHT))
+SCREEN_3 = pygame.display.set_mode((WIDTH, HEIGHT))
+SCREEN_4 = pygame.display.set_mode((WIDTH, HEIGHT))
+SCREEN_5 = pygame.display.set_mode((WIDTH, HEIGHT))
+SCREEN_6 = pygame.display.set_mode((WIDTH, HEIGHT))
+
+BACKGROUND_COLOR = (152, 245, 255)
+SQUARE_COLOR = (83, 134, 139)
+FONT_COLOR = (193, 205, 205)
---
+++ screen_1.py
@@ -0,0 +1,5 @@
+import always
+
+
+def draw_screen_1():
+    always.SCREEN_1.fill()

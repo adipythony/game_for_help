e07a4b7f40bf8d21c02cf66afaf0f574923d52d9
work <adiissan415@gmail.com>
2023-09-20 16:51:08 +0300

    h

--- screen_5.py
+++ screen_5.py
@@ -5,16 +5,16 @@ screen_5 = always.SCREEN_5
 
 def open_screen_5():
     pygame.init()
-    dict_5 = {'running': True}
+    # dict_5 = {'running': True}
     screen_5.fill(always.BACKGROUND_COLOR)
     draw_outside_rect_5()
     draw_iner_rect_5()
     text_5()
     pygame.display.flip()
-    while dict_5['running']:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                dict_5['running'] = False
+    # while dict_5['running']:
+    #     for event in pygame.event.get():
+    #         if event.type == pygame.QUIT:
+    #             dict_5['running'] = False
     return always.SCREEN_5
 
 

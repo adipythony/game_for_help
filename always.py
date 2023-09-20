5f4a47a1b57a96737d2f21f33a26d2f609acf1b9
belle <bell.levi2406@gmail.com>
2023-09-20 15:45:36 +0300

    commit 14

--- always.py
+++ always.py
@@ -15,5 +15,5 @@ INER_SQUARE_COLOR = (173, 216, 230)
 FONT_COLOR = (255, 255, 255)
 FONT_NAME = "Calibri"
 
-SUBJECT_LIST = {"Math": [], "English": [], "Literature": [], "Bible": [], "Citizenship": [], "History": [], "Hebrew": []}
-
+SUBJECT_DICT = {"Math": 0, "English": 1, "History": 2, "Hebrew": 3, "Bible": 4, "Literature": 5, "Citizenship": 6}
+PEOPLE_LIST = [[], [], [], [], [], [], []]

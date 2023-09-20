771622bf7be2a4c8978302f191898256477f2293
maya <mayush.benami@gmail.com>
2023-09-20 16:07:00 +0300

    add thingy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+
+import always
 import screen_1
 import screen_2
 import screen_3
@@ -18,16 +20,22 @@ def event():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             state["running"] = False
-        if event.type == pygame.MOUSEBUTTONUP:
-            pos = pygame.mouse.get_pos()
-            if screen_1.start.collidepoint(pos):
-                screen_2.background()
-                for event2 in pygame.event.get():
-                    if event2.type == pygame.MOUSEBUTTONUP:
-                        pos2 = pygame.mouse.get_pos()
-                        if screen_2.helped.collidepoint(pos2) or screen_2.helper.collidepoint(pos2):
-                            screen_2
+        if always.next == 1:
+            if event.type == pygame.MOUSEBUTTONUP:
+               pos = pygame.mouse.get_pos()
+               if screen_1.start.collidepoint(pos):
+                    screen_2.background()
+                    always.next = 2
+    event_2()
 
+
+def event_2():
+    if always.next == 2:
+        for event2 in pygame.event.get():
+            if event2.type == pygame.MOUSEBUTTONUP:
+                pos2 = pygame.mouse.get_pos()
+                if screen_2.helped.collidepoint(pos2) or screen_2.helper.collidepoint(pos2):
+                    screen_1.draw_screen_1()
 
 
 main()
